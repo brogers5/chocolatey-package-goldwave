c2878c86011c4b3d62656192039f86d4caf5ab73
brogers5 <6869577+brogers5@users.noreply.github.com>
2023-08-07 14:18:55 -0400

    Remove unnecessary version declaration

--- build.ps1
+++ build.ps1
@@ -2,14 +2,11 @@
 
 $currentPath = (Split-Path $MyInvocation.MyCommand.Definition)
 $nuspecFileRelativePath = Join-Path -Path $currentPath -ChildPath 'goldwave.nuspec'
-[xml] $nuspec = Get-Content -Path $nuspecFileRelativePath
-$version = [Version] $nuspec.package.metadata.version
 
 $global:Latest = @{
     FileName64 = 'InstallGoldWave677'
     FileType   = 'exe'
     Url64      = 'https://web.archive.org/web/20230807180028if_/https://goldwave.com/download.php?file=gw'
-    Version    = $version
 }
 
 Write-Output 'Downloading...'
