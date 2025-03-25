d32fa66bd6f7fc6cb1a95643ebcba6f12c8b298d
brogers5 <6869577+brogers5@users.noreply.github.com>
2025-03-25 15:49:42 -0400

    Update to 7.2.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Confirm-Win10
 
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
-$installerFileName = 'InstallGoldWave701.exe'
+$installerFileName = 'InstallGoldWave702.exe'
 $filePath = Join-Path -Path $toolsDir -ChildPath $installerFileName
 $softwareNamePattern = 'GoldWave v*'
 
