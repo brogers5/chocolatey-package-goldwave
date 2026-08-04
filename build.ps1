e6e595b4b47630cada5fd54af8a28783dd678c9b
brogers5 <6869577+brogers5@users.noreply.github.com>
2026-08-04 12:39:06 -0400

    Update to 7.7.0

--- build.ps1
+++ build.ps1
@@ -4,9 +4,9 @@ $currentPath = (Split-Path $MyInvocation.MyCommand.Definition)
 $nuspecFileRelativePath = Join-Path -Path $currentPath -ChildPath 'goldwave.nuspec'
 
 $global:Latest = @{
-    FileName64 = 'InstallGoldWave705'
+    FileName64 = 'InstallGoldWave707'
     FileType   = 'exe'
-    Url64      = 'https://web.archive.org/web/20260729230323if_/https://goldwave.com/download.php?file=gw'
+    Url64      = 'https://web.archive.org/web/20260804163753if_/https://goldwave.com/download.php?file=gw'
 }
 
 Write-Output 'Downloading...'
--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Confirm-Win10
 
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
-$installerFileName = 'InstallGoldWave705.exe'
+$installerFileName = 'InstallGoldWave707.exe'
 $filePath = Join-Path -Path $toolsDir -ChildPath $installerFileName
 $softwareNamePattern = 'GoldWave v*'
 
